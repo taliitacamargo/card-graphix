d9d0a746ab654b330b7bb432d51ecc7e19c22531
Isaac Joshua <isaacjcarnes@gmail.com>
2022-02-09 23:22:53 -0500

    Set up for card fields

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -8,7 +8,10 @@ import '../css/content.css';
 const Home = () => {
     return(
         <main className='CardPage d-flex justify-center align-content-center bg-light'>
-            <div className='CardView justify-center'>Hello</div>
+            <div className='CardView justify-center'>
+                <div className="NameValue" style={{'right': '0.2in', 'top': '0.2in'}}>Isaac Carnes</div>
+                <div className="EmailValue" style={{'right': '0.2in', 'top': '0.4in'}}>isaacjoshuadeveloper@gmail.com</div>
+            </div>
         </main>
     )
 };
