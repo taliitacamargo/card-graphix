7596854409cf6e3270e7571c563a981e53206667
Isaac Joshua <isaacjcarnes@gmail.com>
2022-02-14 21:32:10 -0500

    CardBuilder.js has model card with working component object

--- client/src/components/CardBuilder/index.js
+++ client/src/components/CardBuilder/index.js
@@ -8,14 +8,24 @@ const CardBuilder = () => {
             this.compStyle = compStyle;
         }
     }
-    console.log(JSON.stringify({
-        left: "1.7in",
-        top: "0.1in",
-        fontSize: "20px",
-      }));
+
     let compArray = [
-        new Component("Isaac Carnes", "NameValue", JSON.stringify({
-            left: "1.7in",
+        new Component("", "Background", JSON.stringify({
+            height: "100%",
+            width: "45%",
+            backgroundColor: "black",
+            left: "0in",
+            top: "0in",
+          })),
+        new Component("", "Logo", JSON.stringify({
+            height: "1in",
+            width: "1in",
+            backgroundColor: "",
+            left: "0.3in",
+            top: "0.5in",
+          })),
+        new Component("Mac Jones", "NameValue", JSON.stringify({
+            left: "1.8in",
             top: "0.1in",
             fontSize: "20px",
         })),
@@ -26,35 +36,29 @@ const CardBuilder = () => {
           })),
         new Component("+1 412-111-1111", "Phone1Value", JSON.stringify({
             right: "0.15in",
-            top: "0.55in",
+            top: "0.65in",
+          })),
+        new Component("+1 412-222-2222", "Phone2Value", JSON.stringify({
+            right: "0.15in",
+            top: "0.75in",
+          })),
+        new Component("user56@hotmail.com", "EmailValue", JSON.stringify({
+            right: "0.15in",
+            top: "1.05in",
+          })),
+        new Component("user56.github.io/MyPortfolio/", "WebsiteValue", JSON.stringify({
+            right: "0.15in",
+            top: "1.15in",
+          })),
+        new Component("83 Park Place", "Address1Value", JSON.stringify({
+            right: "0.15in",
+            top: "1.45in",
+          })),
+        new Component("22 Atlantic Avenue", "Address2Value", JSON.stringify({
+            right: "0.15in",
+            top: "1.55in",
           })),
       ];
-    let cardObj = {
-          phoneNum2Field: {
-            value: "+1 412-222-2222",
-            class: "Phone2Value",
-            style: {
-              right: "0.15in",
-              top: "0.65in",
-            },
-          },
-        emailField: {
-          value: "isaacjoshuadeveloper@gmail.com",
-          class: "EmailValue",
-          style: {
-            right: "0.15in",
-            top: "0.85in",
-          },
-        },
-        websiteField: {
-            value: "isaacjcarnes.github.io/react-portfolio-ij/",
-            class: "WebsiteValue",
-            style: {
-              right: "0.15in",
-              top: "0.95in",
-            },
-          },
-      };
 
       const cardComps = compArray.map((item, i) => <div className={item.compClass} style={JSON.parse(item.compStyle)} key={i}>{item.compValue}</div>)
     
