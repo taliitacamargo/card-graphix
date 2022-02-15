0346c41b0422ff080fd594cdfafe42dd5c3cdd4b
Amy Hwang <728amyh@gmail.com>
2022-02-14 19:16:32 -0500

    changed font style

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { useLocation, useHistory } from 'react-router-dom';
+import "./footer.css";
 
 const Footer = () => {
   const location = useLocation();
   const history = useHistory();
   return (
     <footer className="w-100 mt-auto text-dark p-4">
-      <div className="container text-center mb-5">
+      <div className="container text-center mb-5" >
         {location.pathname !== '/' && (
           <button
             className="btn btn-dark mb-3"
