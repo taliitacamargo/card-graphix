37c05160759c7f67e483f90682c15dd496ba2824
Isaac Joshua <isaacjcarnes@gmail.com>
2022-02-14 19:36:07 -0500

    Fixed home.js paths

--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -1,15 +1,11 @@
 import React from "react";
 import { useQuery } from "@apollo/client";
 
-<<<<<<< HEAD:client/src/pages/Home.js
-import CardBuilder from '../components/CardBuilder';
+import CardBuilder from '../../components/CardBuilder';
 
-import { QUERY_PROFILES } from "../utils/queries";
-=======
 import { QUERY_PROFILES } from "../../utils/queries";
->>>>>>> a4f8956a6def455cf427f4d45f828abb65bf186a:client/src/pages/home/Home.js
 
-import "../home/home.css";
+import "./home.css";
 
 const Home = () => {
   return(
