5b75407336d4aa901c8a9ca661de07c70421bb6c
Amy Hwang <728amyh@gmail.com>
2022-02-22 20:24:04 -0500

    added some animation effects

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import "./header.css"
 import Auth from '../../utils/auth';
+import { motion } from "framer-motion";
 
 const Header = () => {
   const logout = (event) => {
@@ -11,9 +12,15 @@ const Header = () => {
  
   return (
     <div className="Header">
+      <motion.div className="inner-header"
+        initial={{ y: -250 }}
+        animate={{ y: -10 }}
+        transition={{ delay: 0.2, type: 'spring', stiffness: 200 }}
+      >
       <Link className="logo" to="/">
         <h1>Card Graphix</h1>
       </Link>
+      </motion.div>
         {Auth.loggedIn() ? (
           <div className="container-2">
           <button className="logout" onClick={logout}>
@@ -21,14 +28,18 @@ const Header = () => {
           </button>
           </div>
         ) : (
-          <><div className="container-2">
+          <><motion.div className="container-2"
+          initial={{ y: -250 }}
+          animate={{ y: -10 }}
+          transition={{ delay: 0.5 }}
+          >
             <Link className="login section" to="/login">
               Login
             </Link>
             <Link className="signup section" to="/signup">
               Signup
             </Link>
-            </div>
+            </motion.div>
           </>
         )}
     </div>
