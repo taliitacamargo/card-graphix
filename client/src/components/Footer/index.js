e7fb82e9a624eece29856baf5cd8059748a10c2a
Amy Hwang <728amyh@gmail.com>
2022-02-24 17:27:42 -0500

    no changes

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom';
 const Footer = () => {
 
   return (
-    <footer className="w-100 mt-auto text-dark p-4">
+    <footer className="footer">
       <div className="container text-center mb-5" >
         <h4>&copy; {new Date().getFullYear()} Card Graphix. All rights reserved. &nbsp;</h4>
         <Link to="/contact-us">Contact Us</Link>
