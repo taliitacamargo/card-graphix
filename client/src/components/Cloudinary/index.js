f293c0d404de33a1f9c02d1d7ae31a14f9d47cde
Mike Pizzica <mike.pizzica@gmail.com>
2022-02-24 20:55:17 -0500

    add api keys via query

--- client/src/components/Cloudinary/index.js
+++ client/src/components/Cloudinary/index.js
@@ -1,13 +1,18 @@
+// setup API call to hold cloudName. holds
 import React, { Component, useEffect } from "react";
+import { useQuery } from '@apollo/client';
+
+import { QUERY_SECRET_KEYS } from '../../utils/queries';
 
 function Cloudinary(props) {
-    useEffect( () => {
-        if(window.cloudinary){
+    const { loading, data } = useQuery(QUERY_SECRET_KEYS);
+        useEffect( () => {
+        if(window.cloudinary && !loading){
             var myWidget = window.cloudinary.createUploadWidget({
-                cloudName: 'card-graphix', 
-                uploadPreset: 'fcep02lt',
+                cloudName: data["secretkeys"]["cloudinary_cloud_name"], 
+                uploadPreset: data["secretkeys"]["cloudinary_upload_preset"],
                 sources: [ 'local', 'image_search', 'instagram', 'facebook', 'url', 'camera'],
-                googleApiKey: 'AIzaSyCZqq4TeSsr3D4FuyKeLxgcV-cmgKwA-no' }, 
+                googleApiKey: data["secretkeys"]["google_api_key"] }, 
                 (error, result) => { 
                     if (!error && result && result.event === "success") { 
                         console.log('Cheetah! Here is the image info: ', result.info); 
@@ -21,7 +26,7 @@ function Cloudinary(props) {
                 }, false);
         }
     }
-    ,[])
+    ,[data])
     return(
         <button id="upload_widget">Upload Logo</button>
     )
